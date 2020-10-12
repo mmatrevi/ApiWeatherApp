4f3e877536863ee4da9ba26235565d6f7aa201b1
unknown <mmatrevi@gmail.com>
2020-10-12 14:53:52 +0000

    update

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-const Forecast = ({setUserLoggedin}) => {
+function Forecast({setUserLoggedin}) {
 
-    let [responseObj, setResponseObj] = useState({});
+   let [responseObj, setResponseObj] = useState({});
 
     function Logoutfunction(){
         setUserLoggedin(false)
@@ -10,27 +10,31 @@ const Forecast = ({setUserLoggedin}) => {
 
    function getForecast() {
       // weather data fetch function will go here
-      fetch("https://community-open-weather-map.p.rapidapi.com/weather?lat=0&lon=0&callback=test&id=2172797&lang=null&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=seattle", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": "9a37cde1acmshd706ad3a51045dep1921a1jsne27a4f2cef2e"
-	}
-})
-       .then(response => response.json())
-       .then(response => {
-           setResponseObj(response)
-       });
+      fetch("http://api.weatherstack.com/historical" , {
+        access_key : '62e0e4da08472ce1fcc145bc5ca5e982',
+        query : 'New York',
+        historical_date : '2015-21-0',
+        
+    })
+    .then(response => response.json())
+    .then(response => {
+    setResponseObj(response)
+    })
+    .catch(err => {
+        console.log(err);
+    })
    }
 
    return (
        // JSX code will go here
+       <div>
        <div>
            <h2>Find Current Weather Conditions</h2>
            <div>
                {JSON.stringify(responseObj)}
            </div>
            <button onClick={getForecast}>Get Forecast</button>
+       </div>
 
            <br></br>
            <button onClick={Logoutfunction}>Logout</button>
--- src/components/home.js
+++ src/components/home.js
@@ -6,17 +6,9 @@ function Home({setUserLoggedin}) {
         setUserLoggedin(false)
     }
 
-        function getForecast() {
-           // weather data fetch function will go here
-        
-        }
-
 
    return(
-        // JSX code will go here
-
-        
-
+ 
            <div className='Searchbox'>   
             <p style={{fontSize:'40px', paddingLeft:'49px'}}>Temp 25degrees</p>
             <p><input style={{margin:'20px',width:'350px', height:'30px'}} type='text' placeholder='enter your cities name'></input></p>
